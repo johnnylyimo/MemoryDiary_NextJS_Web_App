ed351b544d5a9f39d28629b6b519e7b32bab1ed5
johnnylyimo <johnnylyimo@gmail.com>
2022-07-10 15:54:46 +0300

    Wrapped myApp function with Layout component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,12 @@ import type { AppProps } from 'next/app'
 import Layout from '@components/Layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout >
+      <Component {...pageProps} /> 
+    </Layout>
+  )
+  // return <Component {...pageProps} />
 }
 
 export default MyApp
