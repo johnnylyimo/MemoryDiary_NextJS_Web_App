3811c952f79d57464f2a22583fe83f6a0ae230b1
johnnylyimo <johnnylyimo@gmail.com>
2022-07-10 14:44:31 +0300

    Defined head's title

--- components/NextHead.tsx
+++ components/NextHead.tsx
@@ -10,6 +10,7 @@ export default function NextHead({title, description}:NextHeadProps){
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content={description} />
+        <title>{title}</title>
 
     </Head>
 
