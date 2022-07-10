0d42a62fdf1ba9b4c69b3ad8f9f264870e74f155
johnnylyimo <johnnylyimo@gmail.com>
2022-07-10 15:56:11 +0300

    Passed Layout's meta parameter of type description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout meta={{
       title:'Home | MemoryDiary',
-      
+      description:"MemoryDiary's home page"
       }}>
       <Component {...pageProps} /> 
     </Layout>
