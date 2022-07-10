818ccc80c8e7129359b862ccf511e6ba80c55386
johnnylyimo <johnnylyimo@gmail.com>
2022-07-10 14:43:54 +0300

    Defined description meta

--- components/NextHead.tsx
+++ components/NextHead.tsx
@@ -9,6 +9,7 @@ export default function NextHead({title, description}:NextHeadProps){
     <Head>
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content={description} />
 
     </Head>
 
