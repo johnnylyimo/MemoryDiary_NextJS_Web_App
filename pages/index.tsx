2f95a3a498b8fb5b2546a33d88f21cbf3ebddbec
johnnylyimo <johnnylyimo@gmail.com>
2022-07-25 12:45:35 +0300

    Styled div tag set class col span 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,8 +8,8 @@ const Home: NextPage = () => {
         Home
       </h1>
       <section className='grid grid-cols-1 lg:grid-cols-6 gap-6 p-10 self-stretch'>
-        <div>
-          
+        <div className='col-span-2'>
+
         </div>
       </section>
     </main>
