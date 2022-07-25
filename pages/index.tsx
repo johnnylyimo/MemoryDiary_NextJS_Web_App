1aed57a211424a3f2ff8ff1487cfa2cc475008da
johnnylyimo <johnnylyimo@gmail.com>
2022-07-25 12:38:50 +0300

    Styled h1 tag set string text

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ const Home: NextPage = () => {
   return (
     <main className='flex flex-col items-center space-y-4 [#e5cce5] w-full h-full p-8 '>
       <h1 className='text-4xl text-purple-400 font-bold underline'>
-
+        Home
       </h1>
       <section>
         
