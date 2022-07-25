17e272c4598c9c144787e464f84725f29a32444b
johnnylyimo <johnnylyimo@gmail.com>
2022-07-25 12:34:29 +0300

    Initialized h1 tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,9 @@ import Image from 'next/image'
 const Home: NextPage = () => {
   return (
     <main className='flex flex-col items-center space-y-4 [#e5cce5] w-full h-full p-8 '>
+      <h1>
+        
+      </h1>
       <section>
         
       </section>
