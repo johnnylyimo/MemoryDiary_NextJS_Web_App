1dc2eaeba1c9b25cbbeff4b0a536b4b6278e653b
johnnylyimo <johnnylyimo@gmail.com>
2022-07-25 12:36:58 +0300

    Styled h1 tag set text color

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const Home: NextPage = () => {
   return (
     <main className='flex flex-col items-center space-y-4 [#e5cce5] w-full h-full p-8 '>
-      <h1 className='text-4xl'>
+      <h1 className='text-4xl text-purple-400'>
 
       </h1>
       <section>
