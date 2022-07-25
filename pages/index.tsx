899b84032269e042e6f4de0a978e1159ebd926cd
johnnylyimo <johnnylyimo@gmail.com>
2022-07-25 12:36:05 +0300

    Styled h1 tag set font size

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image'
 const Home: NextPage = () => {
   return (
     <main className='flex flex-col items-center space-y-4 [#e5cce5] w-full h-full p-8 '>
-      <h1>
-        
+      <h1 className='text-4xl'>
+
       </h1>
       <section>
         
