af32a7326221af2f5e2fabf2ab392ffba5efcfdb
johnnylyimo <johnnylyimo@gmail.com>
2022-07-31 17:34:42 +0300

    Styled body tag with background color

--- pages/_document.js
+++ pages/_document.js
@@ -3,14 +3,11 @@ import {Html, Head, Main, NextScript} from 'next/document'
 export default function Document(){
     return (
         <Html lang='en'>
-            <Head>
-
-            </Head>
-            <body>
+            <Head></Head>
+            <body className='bg-[#e5cce5] '>
                 <Main />
                 <NextScript />
             </body>
-
         </Html>
     )
 
