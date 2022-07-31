2b7742f95718e7c66374a6955e7cc8de111fb5ec
johnnylyimo <johnnylyimo@gmail.com>
2022-07-31 17:12:33 +0300

    Styled div tag with height

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,7 +31,7 @@ const Home: NextPage = () => {
               </label>
             </form>
         </div>
-        <div className="col-span-4 lg:col-span-3">
+        <div className="col-span-4 lg:col-span-3 h-[50vh]">
 
         </div>
       </section>
