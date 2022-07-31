742c92a71ba0d8bd1b145c76b438442108072c87
johnnylyimo <johnnylyimo@gmail.com>
2022-07-31 16:58:01 +0300

    Added text string with button tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ const Home: NextPage = () => {
               <textarea rows={4} className='block p-2 w-full text-base text-gray-900 bg-[#cc99cc] rounded-lg border-2 border-purple-400' placeholder='Write new memory here ...'>
               </textarea>
               <button>
-                
+                Save
               </button>
 
             </form>
