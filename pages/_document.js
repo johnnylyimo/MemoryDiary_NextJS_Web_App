4a0668c6bd4f4314d6401bb6a2a6720f41f907d2
johnnylyimo <johnnylyimo@gmail.com>
2022-07-31 17:31:46 +0300

    Added body tag in _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,11 @@ export default function Document(){
     return (
         <Html lang='en'>
             <Head>
-                
+
             </Head>
+            <body>
+                
+            </body>
 
         </Html>
     )
