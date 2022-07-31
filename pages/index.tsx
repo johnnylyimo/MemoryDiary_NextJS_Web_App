0c2e5ae2d0801017df3bfdeecb39aac85973a1f6
johnnylyimo <johnnylyimo@gmail.com>
2022-07-31 16:54:26 +0300

    Updated textarea tag's font size

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,8 +21,7 @@ const Home: NextPage = () => {
               <label htmlFor="message" className='block mb-2 text-md font-medium text-gray-900'>
                 Write new memory here below
               </label>
-              <textarea rows={4} className='block p-2 w-full text-sm text-gray-900 bg-[#cc99cc] rounded-lg border-2 border-purple-400' placeholder='Write new memory here ...'>
-
+              <textarea rows={4} className='block p-2 w-full text-base text-gray-900 bg-[#cc99cc] rounded-lg border-2 border-purple-400' placeholder='Write new memory here ...'>
               </textarea>
 
             </form>
