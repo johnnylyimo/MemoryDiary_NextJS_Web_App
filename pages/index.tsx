d46c081d9bafdd4cad9df644393d145b20ba4891
johnnylyimo <johnnylyimo@gmail.com>
2022-08-13 18:17:38 +0300

    Updated styled section tag background-color

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
         Home
       </h1>
       <section className='grid grid-cols-1 lg:grid-cols-6 gap-6 p-10 self-stretch'>
-        <div className='col-span-2 lg:col-span-3 h-[780px] sm:h-[500px] bg-[#cc99cc] ml-12 lg:ml-0 rounded-lg'>
+        <div className='col-span-2 lg:col-span-3 h-[780px] sm:h-[500px] bg-[#ce93d8] ml-12 lg:ml-0 rounded-lg'>
             <h1 className='text-2xl text-center font-bold uppercase pt-4 tracking-wide'>
             World of the day
             </h1>
