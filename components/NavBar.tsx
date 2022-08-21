fb1f4fdc7d9d0b5de220e969e31016b5ac801c84
johnnylyimo <johnnylyimo@gmail.com>
2022-08-22 02:48:42 +0300

    Styled anchor tag with hover text color

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,7 +4,7 @@ export const NavBar = () => {
   return (
     <nav className="flex justify-center">
        <Link href="/">
-            <a className="text-4xl font-bold text-[#ac5eb9dc] underline">
+            <a className="text-4xl font-bold text-[#ac5eb9dc] underline hover:text-[#800080]">
                 Home
             </a>
        </Link>
