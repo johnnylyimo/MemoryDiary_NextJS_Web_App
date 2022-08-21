d0636b59e7b2f3ba0b60e9f59d7bbc344555f353
johnnylyimo <johnnylyimo@gmail.com>
2022-08-22 02:45:57 +0300

    Styled anchor tag with color

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,7 +4,7 @@ export const NavBar = () => {
   return (
     <nav className="flex justify-center">
        <Link href="/">
-            <a className="text-4xl font-bold">
+            <a className="text-4xl font-bold text-[#ac5eb9dc]">
                 Home
             </a>
        </Link>
