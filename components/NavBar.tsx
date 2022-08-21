e08da60a89ee1bd0960e1fee3fb2948e6756330d
johnnylyimo <johnnylyimo@gmail.com>
2022-08-22 02:50:07 +0300

    Styled nav tag wth margin top

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export const NavBar = () => {
   return (
-    <nav className="flex justify-center">
+    <nav className="flex justify-center mt-5">
        <Link href="/">
             <a className="text-4xl font-bold text-[#ac5eb9dc] underline hover:text-[#800080]">
                 Home
