d8224c047fc8b2d62d62fb629576d6c9de65551e
johnnylyimo <johnnylyimo@gmail.com>
2022-08-29 11:23:13 +0300

    Created Footer function

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
 import {MdPhone,  MdLocationPin, MdEmail} from 'react-icons/md'
+
+
+export default function Footer() {
+  return (
+  )
+}
