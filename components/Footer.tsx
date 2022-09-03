510a7e5bb73e73f36e2e2345a6a0bb30afceaa69
johnnylyimo <johnnylyimo@gmail.com>
2022-09-04 01:30:56 +0300

    Styled div tag with class flex justify center

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -55,7 +55,7 @@ export default function Footer() {
                         <MdPhone size={25}/>
                         <p>0715 291029</p>
                     </div>
-                    <div className="flex ">
+                    <div className="flex justify-center ">
                         
                     </div>
                 </div>
