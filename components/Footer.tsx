55287dfcce35d5e1efd3eaee9a13ece5d314aee1
johnnylyimo <johnnylyimo@gmail.com>
2022-09-04 00:17:03 +0300

    Styled image tag with source attribute

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
   return (
     <footer className="w-full mb-6 p-4  bg-[#ce93d8] grid md:grid-cols-5 gap-4">
         <section className="md:col-span-2 text-center">
-            <Image />
+            <Image  src='/brand.png' />
 
         </section>
         
