81145ac7a9660f6162b83cc4c43be71db185e26f
johnnylyimo <johnnylyimo@gmail.com>
2022-09-04 01:28:41 +0300

    Styled phone icon tag with size attribute

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -52,7 +52,7 @@ export default function Footer() {
                         Contact Me
                     </h1>
                     <div className="flex justify-center gap-x-2">
-                        <MdPhone />
+                        <MdPhone size={25}/>
                     </div>
                 </div>
                 
