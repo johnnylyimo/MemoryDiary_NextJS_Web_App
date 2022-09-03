834887c9b9f7c33426a4e07fb5196f9b9461067c
johnnylyimo <johnnylyimo@gmail.com>
2022-09-04 00:41:40 +0300

    Added Link tag

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -26,6 +26,8 @@ export default function Footer() {
                                 Github
                             </a>
                         </Link>
+                        <Link>
+                        </Link>
                     </div>
                     
                 </div>
