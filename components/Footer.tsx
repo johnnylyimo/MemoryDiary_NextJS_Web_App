f1d1e164a8a030d263a39c31cfb77882226b39b9
johnnylyimo <johnnylyimo@gmail.com>
2022-09-04 00:14:10 +0300

    Added first section tag under footer tag

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,6 +7,9 @@ export default function Footer() {
     const year = new Date().getFullYear();
   return (
     <footer className="w-full mb-6 p-4  bg-[#ce93d8] grid md:grid-cols-5 gap-4">
+        <section>
+            
+        </section>
         
     </footer>
 	
