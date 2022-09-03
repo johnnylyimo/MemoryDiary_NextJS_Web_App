24e3777870132ec7398a04adcc3a5c27e4ecda2d
johnnylyimo <johnnylyimo@gmail.com>
2022-09-04 00:13:19 +0300

    Styled footer tag with class grid deskstop

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ import {MdPhone,  MdLocationPin, MdEmail} from 'react-icons/md'
 export default function Footer() {
     const year = new Date().getFullYear();
   return (
-    <footer className="w-full mb-6 p-4  bg-[#ce93d8] grid">
+    <footer className="w-full mb-6 p-4  bg-[#ce93d8] grid md:grid-cols-5">
         
     </footer>
 	
