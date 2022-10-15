953e47823a6d1f81ca94d839937f8daf63ea5a21
johnnylyimo <johnnylyimo@gmail.com>
2022-10-15 18:34:15 +0300

    Created global object

--- utils/prisma.ts
+++ utils/prisma.ts
@@ -1,2 +1,6 @@
 // CREATE PRIMSA INSTANCE TO BE USED TO QUERY DATABASE ACROSS ENTIRE APPLICATION
 import { PrismaClient } from "@prisma/client";
+
+// allow global `var` declarations
+declare global {
+}
