cf2fe165f315349f47d7ad4c44c1cdb440718329
johnnylyimo <johnnylyimo@gmail.com>
2022-10-17 12:05:37 +0300

    Defined MdEdit tag onClick attribute

--- pages/index.tsx
+++ pages/index.tsx
@@ -81,7 +81,7 @@ const Home: NextPage<HomeProps>  = ({memories}) => {
                 }} >
                   <MdNotes size={25}/>
                   <p>{memory}</p>
-                  <MdEdit size={25} className='cursor-pointer'/>
+                  <MdEdit size={25} className='cursor-pointer' onClick={}/>
                 </div>
               ))
             }
