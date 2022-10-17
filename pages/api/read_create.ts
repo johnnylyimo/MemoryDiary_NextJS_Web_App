ce18e17ea2e0ee9c58e2899e54e4cf1113900dbe
johnnylyimo <johnnylyimo@gmail.com>
2022-10-17 09:34:15 +0300

    Created type of ErrorResponse

--- pages/api/read_create.ts
+++ pages/api/read_create.ts
@@ -1,3 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { PrismaClient } from "@prisma/client";
+import { prisma } from '../../utils/prisma'
 import { Memory } from "@prisma/client";
+
+type ErrorResponse = {
+    
+}
