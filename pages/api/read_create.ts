fb8b2aae3c35627042ced9624008925171302898
johnnylyimo <johnnylyimo@gmail.com>
2022-10-17 10:06:34 +0300

    Invoked prisma's create method assigned to variable

--- pages/api/read_create.ts
+++ pages/api/read_create.ts
@@ -1,40 +1,36 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { prisma } from '../../utils/prisma'
+import { prisma } from "../../utils/prisma";
 import { Memory } from "@prisma/client";
 
 type ErrorResponse = {
-    error: Object;
-}
+  error: Object;
+};
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<Memory | Memory[] | ErrorResponse>
-  ) {
-    if (req.method === "GET"){
-        let memories:Memory[] = [];
-        try {
-            memories = await prisma.memory.findMany();
-        } catch (error) {
-            return console.log(error);
-        }
-        if (!memories) {
-            return res.status(404).json({ error: "No memories data found"});
-        }
-        // successfully response, memories data found
-        res.status(200).json(memories);
-    } 
-    else if (req.method === "POST") {
-        const { title, memory } = req.body;
-        let newMemory: Memory;
-        try {
-            
-        } catch (error) {
-            
-        }
-
+  req: NextApiRequest,
+  res: NextApiResponse<Memory | Memory[] | ErrorResponse>
+) {
+  if (req.method === "GET") {
+    let memories: Memory[] = [];
+    try {
+      memories = await prisma.memory.findMany();
+    } catch (error) {
+      return console.log(error);
     }
-    else{
-        return res.status(405).json({ error: "Wrong request"});
+    if (!memories) {
+      return res.status(404).json({ error: "No memories data found" });
     }
-
+    // successfully response, memories data found
+    res.status(200).json(memories);
+  } else if (req.method === "POST") {
+    const { title, memory } = req.body;
+    let newMemory: Memory;
+    try {
+      newMemory = await prisma.memory.create({
+       
+      });
+    } catch (error) {}
+  } else {
+    return res.status(405).json({ error: "Wrong request" });
   }
+}
