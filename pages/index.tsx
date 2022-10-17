11efe2699012a0e1c45ddadd5ce79158715e7dba
johnnylyimo <johnnylyimo@gmail.com>
2022-10-17 10:55:58 +0300

    Created router' s instance variable

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,6 +18,7 @@ interface HomeProps{
 }
 
 const Home: NextPage<HomeProps>  = ({memories}) => {
+  const router =useRouter()
   return (
     <main className='w-full h-full p-4 my-10 grid grid-cols-1 lg:grid-cols-6'>
         <section className='col-span-2 lg:col-span-3 bg-[#ce93d8] rounded-lg m-4 '>
