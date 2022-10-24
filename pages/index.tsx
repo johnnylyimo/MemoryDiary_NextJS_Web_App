57ea3fa9c6d55edf628ef083a38bfdca2a60647b
johnnylyimo <johnnylyimo@gmail.com>
2022-10-24 12:36:20 +0300

    Styled close button tag with Tailwind class background color

--- pages/index.tsx
+++ pages/index.tsx
@@ -129,13 +129,13 @@ const Home: NextPage<HomeProps>  = ({memories}) => {
                 </textarea>
 
                 <div className="w-full flex justify-around">
-                  <button type="submit" className='flex items-center my-5 w-auto px-8 h-10 bg-red-600 font-semibold text-white t rounded-md shadow-md hover:shadow-lg'  onClick={()=>setEditMemoryPopUp(false)}>
+                  <button type="submit" className='my-5 w-auto px-8 h-10 bg-red-600 font-semibold text-white t rounded-md shadow-md hover:shadow-lg'  onClick={()=>setEditMemoryPopUp(false)}>
                     Delete
                   </button>
-                  <button type="submit" className='flex items-center my-5 w-auto px-8 h-10 bg-blue-600 text-white font-semibold rounded-md shadow-md hover:shadow-lg' onClick={()=>submitEditMemory()}>
+                  <button type="submit" className='my-5 w-auto px-8 h-10 bg-blue-600 text-white font-semibold rounded-md shadow-md hover:shadow-lg' onClick={()=>submitEditMemory()}>
                     Save
                   </button>
-                  <button type="submit" onClick={()=>setEditMemoryPopUp(false)}>
+                  <button type="submit" className='bg-[#ac5eb9dc]' onClick={()=>setEditMemoryPopUp(false)}>
                     Close
                   </button>
 
