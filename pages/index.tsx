8047c7099ffcee72e15225da78f1db925ef06bce
johnnylyimo <johnnylyimo@gmail.com>
2022-10-24 12:28:50 +0300

    Added third button tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -135,6 +135,9 @@ const Home: NextPage<HomeProps>  = ({memories}) => {
                   <button type="submit" className='flex items-center my-5 w-auto px-8 h-10 bg-blue-600 text-white font-semibold rounded-md shadow-md hover:shadow-lg' onClick={()=>submitEditMemory()}>
                     Save
                   </button>
+                  <button>
+                    
+                  </button>
 
                 </div>
               </div>
