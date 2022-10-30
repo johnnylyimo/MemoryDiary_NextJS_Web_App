4e357d0cab47f02d948d5823ed52d1eec7f00c1d
johnnylyimo <johnnylyimo@gmail.com>
2022-10-30 14:41:48 +0300

    Defined catch error statement

--- pages/api/update_delete.ts
+++ pages/api/update_delete.ts
@@ -3,28 +3,29 @@ import { prisma } from "../../utils/prisma";
 import { Memory } from "@prisma/client";
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<Memory | Memory[] | {error: Object} | {message: Object} >
-  ) {
-    const {method} = req;
-    const { id, memory, title} = req.body;
+  req: NextApiRequest,
+  res: NextApiResponse<
+    Memory | Memory[] | { error: Object } | { message: Object }
+  >
+) {
+  const { method } = req;
+  const { id, memory, title } = req.body;
 
-    switch(method){
-        case "PUT":
-            try {
-                const memoryToBeEdited:Memory = await prisma.memory.update({
-                    where: {
-                        id,
-                      },
-                      data: {
-                        title: memory.slice(0, memory.length),
-                        memory,
-                      },
-                    });
-                    res.status(200).json(memoryToBeEdited);
-            } catch (error) {
-                
-            }
-
-    }
+  switch (method) {
+    case "PUT":
+      try {
+        const memoryToBeEdited: Memory = await prisma.memory.update({
+          where: {
+            id,
+          },
+          data: {
+            title: memory.slice(0, memory.length),
+            memory,
+          },
+        });
+        res.status(200).json(memoryToBeEdited);
+      } catch (error) {
+        console.error("Update Request error: ", error);
+      }
   }
+}
