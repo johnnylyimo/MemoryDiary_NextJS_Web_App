ba731c74a4bd70cc20eda7b4fcd4255fa45cac3b
johnnylyimo <johnnylyimo@gmail.com>
2022-10-30 14:41:24 +0300

    Defined response output

--- pages/api/update_delete.ts
+++ pages/api/update_delete.ts
@@ -21,6 +21,7 @@ export default async function handler(
                         memory,
                       },
                     });
+                    res.status(200).json(memoryToBeEdited);
             } catch (error) {
                 
             }
