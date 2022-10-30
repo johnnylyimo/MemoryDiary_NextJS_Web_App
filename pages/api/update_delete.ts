6e09d092348be6fca84121ce0224f759e430885c
johnnylyimo <johnnylyimo@gmail.com>
2022-10-30 14:23:50 +0300

    Created handler function

--- pages/api/update_delete.ts
+++ pages/api/update_delete.ts
@@ -1,3 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../utils/prisma";
 import { Memory } from "@prisma/client";
+
+export default async function handler(
+   
+  ) {}
